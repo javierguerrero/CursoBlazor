1ad2a705411ef784670f62d570f70d57890b7320
jguerrero <jguerrero@goiar.com>
2020-03-11 12:57:09 -0300

    componente crear producto

--- Inventory/Business/B_Category.cs
+++ Inventory/Business/B_Category.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using DataAccess;
+using System.Linq;
 
 namespace Business
 {
     public class B_Category
     {
+        public static List<CategoryEntity> CategoryList()
+        {
+            using (var db = new InventoryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
 
+        public void CreateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.Categories.Add(oCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventoryContext())
+            {
+                db.Categories.Update(oCategory);
+                db.SaveChanges();
+            }
+        }
     }
 }
