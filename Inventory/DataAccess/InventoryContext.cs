25f182012f5611f77711f543f3a0787571b6ff09
jguerrero <jguerrero@goiar.com>
2020-03-10 22:03:43 -0300

    creacion de datacontext

---
+++ Inventory/DataAccess/InventoryContext.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class InventoryContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<InputOutputEntity> InOuts { get; set; }
+        public DbSet<WarehouseEntity> Warehouses { get; set; }
+        public DbSet<StorageEntity> Storages { get; set; }
+
+
+        protected override void OnConfiguring (DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=localhost; Database=InventoryDb; User Id=sa; password=NOfumar34");
+            }
+        }
+    }
+}
